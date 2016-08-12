b290110573a2f9973186563af794be1e81271b4d
shaunlloydmedia <shaunlloydmedia@gmail.com>
2016-08-12 03:26:51 +0000

    Created basic interface without background or counter graphics

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute      
+    }
+  );
+
+  $('#info').openModal();
 });
