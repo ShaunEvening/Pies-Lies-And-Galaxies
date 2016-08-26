cf7dd3ffdc9d13acea718acd6dfa542c612dcefc
shaunlloydmedia <shaunlloydmedia@gmail.com>
2016-08-25 20:13:20 -0400

    refactored css, transferred modals to partial files, and clean html files

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
   $('.modal-trigger').leanModal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
-      opacity: .5, // Opacity of modal background
+      opacity: 0.5, // Opacity of modal background
       in_duration: 300, // Transition in duration
       out_duration: 200, // Transition out duration
       starting_top: '4%', // Starting top style attribute
@@ -38,19 +38,19 @@ $(document).ready(function() {
   });
 
   $('#zip-img').on('click', function() {
-    if (queried_party == '') {
+    if (queried_party === '') {
       queried_party = 'Zip';
     }
     addToFunctionCall('zip');
-    updateReadableQueryLine('Zip')
+    updateReadableQueryLine('Zip');
   });
 
   $('#zap-img').on('click', function() {
-    if (queried_party == '') {
+    if (queried_party === '') {
       queried_party = 'Zap';
     }
     addToFunctionCall('zap');
-    updateReadableQueryLine('Zap')
+    updateReadableQueryLine('Zap');
   });
 
   $('#red-pie').on('click', function() {
@@ -59,7 +59,7 @@ $(document).ready(function() {
         if (response.includes("Congratulations")) {
           $('#ending-modal .modal-content h4').html("You've won!");
         } else {
-          $('#ending-modal .modal-content h4').html("You've lost :(")
+          $('#ending-modal .modal-content h4').html("You've lost :(");
         }
         $('#ending-modal .modal-content p').html(response);
         $('#ending-modal').openModal();
@@ -72,7 +72,7 @@ $(document).ready(function() {
         if (response.includes("Congratulations")) {
           $('#ending-modal .modal-content h4').html("You've won!");
         } else {
-          $('#ending-modal .modal-content h4').html("You've lost :(")
+          $('#ending-modal .modal-content h4').html("You've lost :(");
         }
         $('#ending-modal .modal-content p').html(response);
         $('#ending-modal').openModal();
@@ -82,19 +82,19 @@ $(document).ready(function() {
   $('.bldr-obj').on('click', function() {
     var objId = $(this).attr('id');
     $('#query').val($('#query').val() + objId);
-    $('#askee-value').text($('#askee-value').text() + ' ' + $(this).text())
+    $('#askee-value').text($('#askee-value').text() + ' ' + $(this).text());
   });
 
   $('#is').on('click', function() {
     var objId = $(this).attr('id');
     $('#query').val($('#query').val() + ' == ');
-    $('#askee-value').text($('#askee-value').text() + ' ' + $(this).text())
+    $('#askee-value').text($('#askee-value').text() + ' ' + $(this).text());
   });
 
   $('#is-not').on('click', function() {
     var objId = $(this).attr('id');
     $('#query').val($('#query').val() + ' != ');
-    $('#askee-value').text($('#askee-value').text() + ' ' + $(this).text())
+    $('#askee-value').text($('#askee-value').text() + ' ' + $(this).text());
   });
 
   $('#help-btn').on('click', function() {
@@ -105,8 +105,8 @@ $(document).ready(function() {
     $('#ending-modal .modal-content h4').html("You gave up!?!");
     $.get("http://localhost:3000/game/give-up")
       .done(function(response) {
-        var modalText = "Really? Just no pie at all? What a waste!<br/><br/>This was the game state:<br/><br/>"
-          + response + "<br/>Why not try again?"
+        var modalText = "Really? Just no pie at all? What a waste!<br/><br/>This was the game state:<br/><br/>" +
+            response + "<br/>Why not try again?";
         $('#ending-modal .modal-content p').html(modalText);
         $('#ending-modal').openModal();
       });
@@ -121,25 +121,25 @@ var closeOpenBrackets = function(input) {
       input += ')';
     }
   }
-  return input
-}
+  return input;
+};
 
 var parseQuestion = function() {
   var endOfFirstIf = $('#askee-value').text().indexOf("f");
   return $('#askee-value').text().substr(endOfFirstIf - 1);
-}
+};
  var addToFunctionCall = function(alien) {
    var contents = $('#function-call').val();
    $('#function-call').val(contents + alien + '.ask(');
- }
+ };
 
  var updateReadableQueryLine = function(alien) {
    var askeeLine = $('#askee-value').text();
    if (askeeLine.includes("<Clicking aliens includes them in the query>")) {
      askeeLine = askeeLine.replace("<Clicking aliens includes them in the query>", "Ask " + alien + " if");
-     $('#askee-value').text(askeeLine)
+     $('#askee-value').text(askeeLine);
    } else {
-     askeeLine = askeeLine.replace("if", "to ask " + alien + " if")
-     $('#askee-value').text(askeeLine)
+     askeeLine = askeeLine.replace("if", "to ask " + alien + " if");
+     $('#askee-value').text(askeeLine);
    }
- }
+ };
